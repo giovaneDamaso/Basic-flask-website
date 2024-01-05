35c73325446e160cc069fe1d1ac1b756aaea93a4
Giovane Dâmaso <127995277+Giovane-Damaso@users.noreply.github.com>
2024-01-05 08:35:36 -0300

    Initial Flask App created

---
+++ main.py
@@ -0,0 +1,9 @@
+#import from the aplication created to run
+from website import create_app
+
+app = create_app()
+
+#run the website not the webserver
+if __name__ == '__main__':
+    #with every change made rerun the server
+    app.run(debug=True)
