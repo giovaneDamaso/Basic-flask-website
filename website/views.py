61692e68c1c10830e7996006d8ab3f22526bbe16
Giovane Dâmaso <127995277+Giovane-Damaso@users.noreply.github.com>
2024-01-05 20:47:30 -0300

    Jinja template htlm created

--- website/views.py
+++ website/views.py
@@ -1,9 +1,9 @@
 #views = routes
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 views = Blueprint('views', __name__)
 
 #defining routes
 @views.route('/')
 def home():
-    return "<h1>Test</h1>"
+    return render_template("home.html")
