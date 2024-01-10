7412f348dcf615e995cee74ea2e5ed6d360495ea
Giovane Dâmaso <127995277+Giovane-Damaso@users.noreply.github.com>
2024-01-10 13:18:23 -0300

    Database created

--- website/__init__.py
+++ website/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from os import path
 
 db = SQLAlchemy()
 DB_NAME = 'database.db'
@@ -17,4 +18,17 @@ def create_app():
     app.register_blueprint(views, url_prefix = '/')
     app.register_blueprint(auth, url_prefix = '/')
 
+    from .models import User, Note
+
+    with app.app_context():
+        db.create_all()
+
+    # create_database(app) - not necessary in sqlalchemy 3
+
     return app
+
+# - not necessary anymore in sqlalchemy 3
+# def create_database(app):
+#     if not path.exists('website/' + DB_NAME):
+#         db.create_all(app=app)
+#         print('Created Database!')
